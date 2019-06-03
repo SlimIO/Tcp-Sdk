af4c51197f1b0b3e135ab7f9a92616668af6b9b0
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-06-03 20:27:09 +0200

    refactor: improve d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
 /// <reference types="@slimio/safe-emitter" />
 
 import * as SafeEmitter from "@slimio/safe-emitter";
+import * as Net from "net";
 
 declare namespace TcpClient {
     interface ConstructorOptions {
@@ -13,23 +14,39 @@ declare namespace TcpClient {
         location: string;
         version: string;
     }
+
+    type Calback = { [key: string]: any };
+
+    // TODO how to compose from Addon ? (instead of creating a whole thing here...)
+    interface Targets {
+        "gate.get_info": null;
+        "gate.start": null;
+        "gate.stop": null;
+        "gate.health_check": null;
+    }
+
+    interface Events {
+        connect: void;
+    }
 }
 
-declare class TcpClient extends SafeEmitter {
+declare class TcpClient<T extends TcpClient.Calback = TcpClient.Targets> extends SafeEmitter<TcpClient.Events> {
+
     constructor(options?: TcpClient.ConstructorOptions);
 
     public readonly agent: TcpClient.AgentInfo | null;
     public static DEFAULT_PORT: number;
-    public static modules: TcpClient.DefaultModules;
-    public client: NodeJS.Socket;
+    public client: Net.Socket;
     public host: string;
     public port: string;
 
     getActiveAddons(): Promise<String[]>;
     ping(): Promise<void>;
     connect(): Promise<void>;
-    sendMessage(target: string, args?: any[]): ZenObservable.ObservableLike<any>;
-    sendOne(target: string, args: any): Promise<any>;
+
+    // TODO: how to handle the return type on T ?
+    sendMessage<K extends keyof T>(target: K, args?: T[K]): ZenObservable.ObservableLike<any>;
+    sendOne<K extends keyof T>(target: K, args?: T[K]): Promise<any>;
     close(): void;
 }
 
